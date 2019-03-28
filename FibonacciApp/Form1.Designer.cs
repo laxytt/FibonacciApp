6d1bea73f7cada18b3678441d1ae5e188ca2e044
Wiktor Kukulski <wiktor.kukulski@o365.us.edu.pl>
2019-03-28 20:21:21 +0100

    Dodaj pliki projektów.

---
+++ FibonacciApp/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace FibonacciApp
+{
+    partial class FibonacciSeries
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nValue = new System.Windows.Forms.TextBox();
+            this.sumValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(2, 1);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Fn = (Fn-2) + (Fn-1)";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(630, 311);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(2, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(384, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(392, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Set n value";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nValue
+            // 
+            this.nValue.Location = new System.Drawing.Point(392, 346);
+            this.nValue.Name = "nValue";
+            this.nValue.Size = new System.Drawing.Size(117, 20);
+            this.nValue.TabIndex = 3;
+            // 
+            // sumValue
+            // 
+            this.sumValue.Location = new System.Drawing.Point(515, 346);
+            this.sumValue.Name = "sumValue";
+            this.sumValue.Size = new System.Drawing.Size(117, 20);
+            this.sumValue.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(515, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sequence\'s sum";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FibonacciSeries
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 370);
+            this.Controls.Add(this.sumValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "FibonacciSeries";
+            this.Text = "FibonacciApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nValue;
+        private System.Windows.Forms.TextBox sumValue;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FibonacciApp/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FibonacciApp
+{
+    public partial class FibonacciSeries : Form
+    {
+        public FibonacciSeries()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static int fib(int n)                                         // Funkcja liczy n-ty wyraz ciągu fibonacciego
+        {
+            if (n == 0) return 1;
+            if (n == 1) return 2;
+            return fib(n - 1) + fib(n - 2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)               // Generuje wydarzenie dla przycisku Generate
+        {
+         
+            try                                                              // Obsługa wyjątku(błędu)
+            {
+                int Fn = 0;                                                  // zmienna, dla początkowego wyrazu ciągu
+                int n = Convert.ToInt16(nValue.Text);                        // przypisuje zmiennej n wartość z textBoxa dla n
+
+
+
+                for (int i = 0; i <= n; i++)                                 // pętla działająca dopóki iterowane i nie będzie mniejsze lub równe od n
+                {
+
+                    int y = fib(i);                                          // inicjiuje i przypisuje zmiennej x wartosc ciągu dla i
+                    Fn += fib(i);                                            // fn = fn+fib(i), sumuje wszystkie wyrazy ciągu
+                    chart1.Series[0].Points.AddXY(i + " n", y);              // rysuje wykres o wartosci x =i oraz y=y
+                }
+                sumValue.Text = (Fn).ToString();                               // przypisuje nam wartość Fn do okienka
+
+
+
+
+            }catch (Exception)                                                 // catch wyłapuję bład 
+            {
+                MessageBox.Show("Podaj wartości liczbowe");                    // Wyyświetla messegeBox z komunikatem o błędzie
+            }
+        }
+    }
+    
+}
